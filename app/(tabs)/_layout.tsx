47cbd8f949a5bc39c21724913d872c316628ed10
wuxianaisai <wuxianaisai@mail.ru>
2026-03-25 19:40:05 +0300

    init2

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,43 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarInactiveTintColor: "#CDCDE0",
+            tabBarStyle: {
+                backgroundColor: "fff",
+                borderTopWidth: 1,
+                borderTopColor: "F0F0F0",
+                height: 56,
+                paddingTop: 8
+            }
+        }}>
+            <Tabs.Screen name="index" options={{
+                tabBarIcon: ({ color, focused }) =>
+                     <Ionicons name={focused ? "home" : "home-outline"} size={26}
+                color={color} />
+            }} />
+            <Tabs.Screen name="cart" options={{
+                tabBarIcon: ({ color, focused }) =>
+                     <Feather name={focused ? "shopping-cart" : "shopping-cart"} size={26}
+                color={color} />
+            }} />
+            <Tabs.Screen name="favorites" options={{
+                tabBarIcon: ({ color, focused }) =>
+                     <Ionicons name={focused ? "heart" : "heart-outline"} size={26}
+                color={color} />
+            }} />
+            <Tabs.Screen name="profile" options={{
+                tabBarIcon: ({ color, focused }) =>
+                     <Ionicons name={focused ? "person" : "person-outline"} size={26}
+                color={color} />
+            }} />
+        </Tabs> 
+    )
 }
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-export default function Home() {
+export default function Profile() {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Profile</Text>
         </View>
     )
 }
